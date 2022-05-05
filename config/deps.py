556297f092eb6748eb8b80bb2e25c204b22cd341
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-05 21:48:37 +0300

    fix build

--- config/deps.py
+++ config/deps.py
@@ -1,2 +1,8 @@
 packages=[
+    # for ruby(1) and the core langauge
+    'ruby',
+    # for compiling C modules for ruby
+    'ruby-dev',
+    # for Ruby on Rails
+    'rails',
 ]
---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "demos-ruby"
+project_long_description = "Demos for the ruby language"
+project_year_started = 2015
+project_description = """Demos for the ruby language"""
